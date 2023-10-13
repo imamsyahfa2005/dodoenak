55bdc4379da11f8c36aedfbd286541d3726037a3
root <bobwatcherx@gmail.com>
2023-10-13 14:34:03 +0700

    213

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,8 +1,70 @@
 <script >
-import Indo from '../component/Indo.svelte'	
+import Indo from '../component/Indo.svelte' 
+import Jepang from '../component/Jepang.svelte' 
+import Barat from '../component/Barat.svelte'	
+ 
+
+ let activeTab = 1; // Menentukan tab aktif secara default
+
+  function setActiveTab(tabNumber) {
+    activeTab = tabNumber;
+  }
+
 </script>
 
 
 <div>
-	<Indo/>
+
+<div class="tab-container" style="margin:10px">
+  <div class="tab " on:click={() => setActiveTab(1)}>INDO</div>
+  <div class="tab" on:click={() => setActiveTab(2)}>Jepang</div>
+<div class="tab" on:click={() => setActiveTab(3)}>Barat</div>
+
+  <!-- Tambahkan tab lain sesuai kebutuhan -->
 </div>
+
+	<div class="content">
+  {#if activeTab === 1}
+  <div style="margin:10px">
+  	<h5 style="color:#f5b505;font-weight: bold;">Bokep Indo</h5>
+  </div>
+    <Indo />
+  {:else if activeTab === 2}
+     <div style="margin:10px">
+    <h5 style="color:#f5b505;font-weight: bold;">Bokep Jepang</h5>
+  </div>
+    <Jepang />
+  {:else if activeTab === 3}
+    <div style="margin:10px">
+    <h5 style="color:#f5b505;font-weight: bold;">Bokep Barat</h5>
+  </div>
+    <Barat />
+    
+  <!-- Tambahkan blok lain untuk komponen tambahan -->
+  {/if}
+</div>
+</div>
+
+<style>
+  .tab-container {
+    display: flex;
+    overflow-x: auto; /* Aktifkan scroll horizontal */
+    white-space: nowrap; /* Hindari wrapping ke baris berikutnya */
+  }
+
+  .tab {
+    padding: 10px 20px;
+    cursor: pointer;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-right: 10px;
+    font-weight: bold;
+  }
+
+  
+
+  .content {
+    width: 100%; /* Pastikan konten memenuhi lebar */
+    overflow-x: hidden; /* Sembunyikan scrollbar horizontal di dalam konten */
+  }
+</style>
--- src/lib/pages/Player.svelte
+++ src/lib/pages/Player.svelte
@@ -1,1 +1,120 @@
-player
+<script>
+  export let id;
+  import { onMount } from 'svelte';
+  import { Link } from 'svelte-navigator';
+  let loading = true;
+  let relatedVideos = [];
+
+  async function related() {
+    loading = true; // Menampilkan indikator loading
+
+    try {
+      const response = await fetch("https://poophdserver.vercel.app/relatedpost?key=raQu2lrd&limit=25");
+      const data = await response.json();
+      relatedVideos = data.videos;
+    } catch (error) {
+      console.error("Gagal mengambil video terkait:", error);
+    } finally {
+      loading = false; // Sembunyikan indikator loading setelah data diambil
+    }
+  }
+
+  onMount(() => {
+    related();
+  });
+
+  function gotopage(myid) {
+    id = myid;
+    related();
+  }
+</script>
+
+<div>
+	<div class="container" style="margin-top: 10px;">
+	<Link to="/" class="waves-effect btn "
+	style="background-color: #e68c07;font-weight: bold;"
+	>
+	Kembali
+</Link>
+</div>
+	<div style="margin-top:10px">
+		<iframe src={`https://poop.mom/e/${id}`}
+		allowfullscreen width="100%" height="280px"
+		allowscrolling="no" 
+		></iframe>
+	</div>
+
+	<!-- TOMBOL DOWNLOAD -->
+	<div style="margin:10px">
+		<button 
+		disabled 
+		class="waves-effect btn"
+		style="width: 100%;
+		background-color: #e68c07;color:black;font-weight: bold;
+		"> Download Video belum Tersedia </button>
+	</div>
+
+	<!-- RELATED -->
+	<div style="margin:10px">
+		<h5 style="font-weight:bold;color: white;">Video Lainnya</h5>
+	</div>
+
+		<div id="related" class="row">
+	{#if loading}
+		    <div style="display:flex;flex-direction:column;justify-content:center;
+		  margin: 50px;
+		"
+		> 
+		<div class="preloader-wrapper active">
+		  <div class="spinner-layer spinner-orange-only">
+		    <div class="circle-clipper left">
+		      <div class="circle"></div>
+		    </div><div class="gap-patch">
+		      <div class="circle"></div>
+		    </div><div class="circle-clipper right">
+		      <div class="circle"></div>
+		    </div>
+		  </div>
+		</div>
+		<br>
+		<h5 style="color:white">Sedang Mencari data ....</h5>
+		<br>
+		<p>Jika Terlalu lama bisa refresh kembali atau close buka lagi</p>
+		</div>
+	{:else}
+	  {#each relatedVideos as video (video.id)}
+	    <div class="col m4 xl4 s12" >
+	      <div class="card" on:click={()=>gotopage(video.id)}>
+	        <div class="card-image">
+	          <img src={video.image} alt={video.title} style="width:100%;background-size:cover;">
+	        </div>
+	        <div class="card-content" style="color:white;background-color:black">
+	          <span style="font-size: 15px;font-weight: bold">{video.title}</span>
+	          <p>Durasi: {video.duration}</p>
+	          
+	          <div style="display:flex;justify-content:space-evenly;">
+	          	<p>Views: {video.views}</p>
+	          <p>Tanggal: {video.date_formatted}</p>
+	          </div>
+	        </div>
+	        <div  style="background-color:#ff9903;padding:10px">
+	          <button 
+	          style="color: black;font-weight: bold;
+	          text-align: center;font-size: 18px;
+	          background-color:#ff9903;border:0px;
+	          "
+	          on:click={()=>gotopage(video.id)}>Nonton Sekarang</button>
+	        </div>
+	      </div>
+	    </div>
+	  {/each}
+	 {/if}
+	</div>
+</div>
+
+<style>
+  
+  .spinner-orange-only .circle {
+  border-color: #f0e6cc ;
+}
+</style>
