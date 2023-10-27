f8ad71e845f4cead7c04e7aa6b00fd3ca2b0da7f
root <bobwatcherx@gmail.com>
2023-10-27 10:08:37 +0700

    231

--- src/lib/pages/Request.svelte
+++ src/lib/pages/Request.svelte
@@ -12,6 +12,6 @@
 	</div>
 </div>
 
-<div style="margin-top:20px">
-	<iframe src="https://bobwatcherx.github.io/requestbo/" width="100%" height="500px" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
+<div style="margin-top:20px;background-color: white;">
+	<iframe src="https://bobwatcherx.github.io/requestbo/" width="100%" height="700px" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
 </div>
