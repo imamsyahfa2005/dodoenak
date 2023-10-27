c032afa1c4757522f20b17dfbfbde2041bc2facc
root <bobwatcherx@gmail.com>
2023-10-27 10:06:18 +0700

    32

--- src/lib/pages/Request.svelte
+++ src/lib/pages/Request.svelte
@@ -8,10 +8,10 @@
 	style="background-color: #ffa805;color: black;"
 	>Kembali</Link>
 	<div >
-		<p>Kami Selaku Pengepul bokep . akan selalu memberikan bokep terupdate , termontok , terbohai setiap hari . jadi jangan lupa untuk pantau web ini . yang mau request </p>
+		<p>Demi Anak Sayang Bokep . Kami Selaku Pengepul bokep . akan selalu memberikan bokep terupdate , termontok , terbohai setiap hari . jadi jangan lupa untuk pantau web ini . yang mau request </p>
 	</div>
 </div>
 
 <div style="margin-top:20px">
-	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdg5uJXp9FrmFmX8hqjpsklmeFy4EKhGpE0z3BDyQ_6w-SXIg/viewform?embedded=true" width="100%" height="375" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
+	<iframe src="https://bobwatcherx.github.io/requestbo/" width="100%" height="500px" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
 </div>
