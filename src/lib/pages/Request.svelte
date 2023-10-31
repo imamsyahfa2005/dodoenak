18d18e4c7c2443c8618aaf85defcf369c2004653
root <bobwatcherx@gmail.com>
2023-10-31 09:28:09 +0700

    3

--- src/lib/pages/Request.svelte
+++ src/lib/pages/Request.svelte
@@ -4,9 +4,23 @@
 
 <div class="container" style="margin-top:20px">
 
-	<Link to="/" class="waves-effect btn"
-	style="background-color: #ffa805;color: black;"
-	>Kembali</Link>
+	<div style="display:flex;justify-content:space-between;">
+		<Link to="/" class="waves-effect btn "
+			style="background-color: #e68c07;font-weight: bold;"
+			>
+				Kembali
+			</Link>
+			<Link to="/caribokep" class="waves-effect btn "
+			style="background-color: #5eff08;font-weight: bold;color:black"
+			>
+				Cari Bokep
+			</Link>
+	</div>
+	<div style="margin:10px">
+		 <Link to="/permintaan" class="btn "
+    style="width: 100%;background-color: #fa0ae2;font-weight: bold;"
+  >Request Kalian Disini</Link>
+	</div>
 	<div >
 		<p>Demi Anak Sayang Bokep . Kami Selaku Pengepul bokep . akan selalu memberikan bokep terupdate , termontok , terbohai setiap hari . jadi jangan lupa untuk pantau web ini . yang mau request </p>
 	</div>
